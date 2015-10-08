28ca7ca45fc0c6afa22ce46c6d8171a0a75948b2
nptrsn <nick@hindiemedia.com>
2015-10-07 22:52:34 -0700

    bg, files

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 
 
 get_header(); ?>
-
+<body class="home-page">
+  <video autoplay loop muted poster="vid-first-frame.jpg" class="home-bg">
+    <source src="media/ls/mov" type="video/mov">
+  </video>
+</body>
 
 <?php get_footer(); ?>
